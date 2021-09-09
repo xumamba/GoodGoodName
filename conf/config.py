b76c83bdf68a396f73c87b63f54159c8dc98d193
xuliangpan <xuliangpan@didiglobal.com>
2021-09-09 10:54:50 +0800

    Feat: 修改配置

--- conf/config.py
+++ conf/config.py
@@ -1,11 +1,11 @@
 # 强烈建议修改
-LAST_NAME = '廖'  # 姓氏
+LAST_NAME = '孙'  # 姓氏
 SEX = '女'  # 孩子性别，男 或者 女
-year = 2018  # 出生的时间：年
-month = 8  # 出生的时间：月
-date = 19  # 出生的时间：日
-hour = 18  # 出生的时间：小时
-minute = 25  # 出生的时间： 分钟
+year = 2021  # 出生的时间：年
+month = 9  # 出生的时间：月
+date = 6  # 出生的时间：日
+hour = 00  # 出生的时间：小时
+minute = 3  # 出生的时间： 分钟
 
 # 选择性修改
 MIN_SINGLE_NUM = 2  # 单个字最少笔画过滤
@@ -18,18 +18,9 @@ debug = False
 my_write_num_list = [(7, 10)]  # 经过第一轮测试后笔画的结果， 自己记录下来
 true_request = True  # 真实请求测试
 # 名字固定要的字
-fix_write_word = '宸'
+fix_write_word = ''
 SELECTED_SANCAI = ['大吉', '中吉']  # 三才中，如果为None就不特意选最好的
 
 # 首先在http://www.qimingzi.net/ 网站提交基本信息，点击开始起名，F12查看请求信息把Cookie复制下来
-headers = {"Cookie": "bdshare_firstime=1535117517052; Hm_lvt_4baf75bdd37d2c14c33b580be5423f7f=1535117517,1535367603; "
-                     "__tins__5033285=%7B%22sid%22%3A%201535367603157%2C%20%22vd%22%3A%206%2C%20%22expires%22%3A"
-                     "%201535369610438%7D; __51cke__=; __51laig__=6; "
-                     "Hm_lpvt_4baf75bdd37d2c14c33b580be5423f7f=1535367811; userSurname=%e5%bb%96; userSex=2; "
-                     "searchType=report; otherPara=%e5%b9%bf%e4%b8%9c%e7%9c%81%e6%b7%b1%e5%9c%b3%7c10%7c%e6%9c%a8%7c"
-                     "%3cb%3e%e4%ba%94%e8%a1%8c%e5%88%86%e6%9e%90%3c%2fb%3e%ef%bc%9a%e5%85%ab%e5%ad%97%e8%bf%87%e7%a1"
-                     "%ac%ef%bc%8c%e5%85%ab%e5%ad%97%e5%96%9c%e6%9c%a8%ef%bc%8c%e8%b5%b7%e5%90%8d%e6%9c%80%e5%a5%bd"
-                     "%e7%94%a8%e4%ba%94%e8%a1%8c%e5%b1%9e%e6%80%a7%e4%b8%ba%e3%80%8c%3cfont+color%3d0033FF%3e%3cb%3e"
-                     "%e6%9c%a8%3c%2fb%3e%3c%2ffont%3e%e3%80%8d%e7%9a%84%e5%ad%97%3cbr%3e; year=2018; month=8; "
-                     "date=19; hour=18; minute=25",
+headers = {"Cookie": "__51cke__=; Hm_lvt_1f1b125fd1b03fdb6cac5abdd0f5d306=1603866097,1603882167; ASP.NET_SessionId=typwis2xwc1cm5kd2iehtskw; __tins__20674741=%7B%22sid%22%3A%201603952255714%2C%20%22vd%22%3A%203%2C%20%22expires%22%3A%201603954232685%7D; 53gid2=10308951834010; visitor_type=new; 53gid0=10308951834010; 53gid1=10308951834010; 53revisit=1603952432864; 53kf_72241622_from_host=www.qimingzi.net; 53kf_72241622_keyword=; 53kf_72241622_land_page=http%253A%252F%252Fwww.qimingzi.net%252FnameReport.aspx%253Fsurname%253D%2525D3%2525DA%2526name%253D%2525D7%2525CE%2525E8%2525F2%2526sex%253D%2525C5%2525AE; kf_72241622_land_page_ok=1; 53uvid=1; onliner_zdfq72241622=0; __tins__5033285=%7B%22sid%22%3A%201603952125189%2C%20%22vd%22%3A%2013%2C%20%22expires%22%3A%201603955633053%7D; __51laig__=19; Hm_lpvt_1f1b125fd1b03fdb6cac5abdd0f5d306=1603953833",
            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:61.0) Gecko/20100101 Firefox/61.0"}
